7022b0d8cb1423aa723b6889922805edfe721863
Edwin HC <edwinrhc@gmail.com>
2025-11-26 19:50:26 -0500

    feat: Inicializar estructura del proyecto microservicio reactivo - Agregar paquetes base: api, service: repository, domain - Configurar Gradle con WebFlux, R2DBC, Validation, Lombok - Configurar Java 17

---
+++ src/main/java/com/erhc/alumnoservice/app/CodeChallengeDsApplication.java
@@ -0,0 +1,13 @@
+package com.erhc.alumnoservice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengeDsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeChallengeDsApplication.class, args);
+    }
+
+}
